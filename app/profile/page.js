b22df4501f55dccdb47042aa62c8e36a09f1c204
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2024-07-03 12:12:22 -0400

    first deploy

---
+++ app/profile/page.js
@@ -0,0 +1,12 @@
+const page = () => {
+    return (
+        <div>
+
+            <span>
+                <h1>Profile page</h1>
+            </span>
+        </div>
+    );
+}
+
+export default page;
